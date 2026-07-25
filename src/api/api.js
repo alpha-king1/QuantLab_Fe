66bcdd01a669b01cf393511cafab13e0419fd23a
John Oduyebo <79099498+alpha-king1@users.noreply.github.com>
2026-07-25 13:50:26 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://quantlab-k9yr.onrender.com";
 
 export async function getTradingPairs() {
   const response = await fetch(`${API_BASE_URL}/trading_pairs`);
